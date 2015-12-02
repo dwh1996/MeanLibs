d49b64416223ce5b0f3d4e3560dec2aa033ab161
dwh1996 <dwh1996@gmail.com>
2015-12-01 19:46:08 -0500

    Hopefully completed the backend

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express(); // Define our app using express
 var bodyParser = require('body-parser'); // Get body-parser
 var morgan = require('morgan'); // Used to see requests
 var mongoose = require('mongoose'); // For working w/ our database
+var Lib = require('lib.js');
 
 // App Configuration
 // Use body parser so we can grab information from POST requests
@@ -27,6 +28,26 @@ mongoose.connect('localhost:27017/test');
 // Set static files location
 // Used for requests that our frontend will make
 app.use(express.static(__dirname + '/public'));
+
+app.post('/lib', function(req, res) {
+  // Make the object
+  var thelib = new Lib();
+
+  // Set the stuff in the lib
+  thelib.firstWord = req.body.firstWord;
+  thelib.secondWord = req.body.secondWord;
+  thelib.thirdWord = req.body.thirdWord;
+  thelib.fourthWord = req.body.fourthWord;
+  thelib.fifthWord = req.body.fifthWord;
+
+  User.find(function(err, libs) {
+    if (err) res.send(err);
+
+    // Return the whole thing of text
+    res.json(libs[0] + thelib.firstWord + libs[1] + thelib.secondWord + libs[2] +
+            thelib.thirdWord + libs[3] + thelib.fourthWord + libs[4] + thelib.fifthWord + libs[5]);
+  });
+});
 
 // Main catchall routes
 // Send users to frontend
